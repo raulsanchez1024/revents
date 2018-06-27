75c2e02371138367d4107f09651d4deefcdb9ec1
Raul <raulsanchez1024@gmail.com>
2018-06-27 14:14:19 -0600

    Worked on the nav auth cases and added all the stateless components

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,33 +1,60 @@
 import React, { Component } from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
+import { NavLink, Link, withRouter } from 'react-router-dom';
+import SignedInMenu from '../Menus/SignedInMenu';
+import SignedOutMenu from '../Menus/SignedOutMenu';
 
 // alt-shift-f -> formats all the code manually
 class NavBar extends Component {
+  state = {
+    auth: false
+  };
+
+  handleSignIn = () => {
+    this.setState({
+      auth: true
+    });
+  };
+
+  handleSignOut = () => {
+    this.setState({
+      auth: false
+    });
+    this.props.history.push('/');
+  };
+
   render() {
+    const { auth } = this.state;
     return (
       <Menu inverted fixed="top">
         <Container>
-          <Menu.Item header>
+          <Menu.Item as={Link} to="/" header>
             <img src="assets/logo.png" alt="logo" />
             Revents
           </Menu.Item>
-          <Menu.Item name="Events" />
-          <Menu.Item>
-            <Button floated="right" positive inverted content="Create Event" />
-          </Menu.Item>
-          <Menu.Item position="right">
-            <Button basic inverted content="Login" />
-            <Button
-              basic
-              inverted
-              content="Sign Out"
-              style={{ marginLeft: '0.5em' }}
-            />
-          </Menu.Item>
+          <Menu.Item as={NavLink} to="/events" name="Events" />
+          {auth && <Menu.Item as={NavLink} to="/people" name="People" />}
+          {auth && (
+            <Menu.Item>
+              <Button
+                as={Link}
+                to="/createEvent"
+                floated="right"
+                positive
+                inverted
+                content="Create Event"
+              />
+            </Menu.Item>
+          )}
+          {auth ? (
+            <SignedInMenu signOut={this.handleSignOut} />
+          ) : (
+            <SignedOutMenu signIn={this.handleSignIn} />
+          )}
         </Container>
       </Menu>
     );
   }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
@@ -7,7 +8,12 @@ import registerServiceWorker from './registerServiceWorker';
 
 const rootElement = document.getElementById('root');
 let render = () => {
-    ReactDOM.render(<App />, rootElement);
+    ReactDOM.render(
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>, 
+        rootElement
+    );
 }
 
 // This allows for updating content without having to
