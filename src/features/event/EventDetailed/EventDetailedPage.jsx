7e287730ea585ff2b27666fc225d145e504292ad
Raul <raulsanchez1024@gmail.com>
2018-07-15 22:03:39 -0600

    Started working on EventDetailPage

--- src/features/event/EventDetailed/EventDetailedPage.jsx
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
+import EventDetailHeader from './EventDetailHeader';
+import EventDetailInfo from './EventDetailInfo';
+import EventDetailChat from './EventDetailChat';
+import EventDetailSidebar from './EventDetailSidebar';
+
+const event = {
+  id: '1',
+  title: 'Trip to Tower of London',
+  date: '2018-03-27',
+  category: 'culture',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+  city: 'London, UK',
+  venue: "Tower of London, St Katharine's & Wapping, London",
+  hostedBy: 'Bob',
+  hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+  attendees: [
+    {
+      id: 'a',
+      name: 'Bob',
+      photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+    },
+    {
+      id: 'b',
+      name: 'Tom',
+      photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+    }
+  ]
+}
 
 const EventDetailedPage = () => {
   return (
-    <div>
-      <h1>Event Detailed Page</h1>
-    </div>
+    <Grid>
+      <Grid.Column width={10}>
+        <EventDetailHeader event={event} />
+        <EventDetailInfo event={event} />
+        <EventDetailChat />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <EventDetailSidebar attendees={event.attendees} />
+      </Grid.Column>
+    </Grid>
   );
-}
+};
 
 export default EventDetailedPage;
---
+++ src/features/event/eventConstants.jsx
@@ -0,0 +1,3 @@
+export const CREATE_EVENT = 'CREATE_ENVET';
+export const UPDATE_EVENT = 'UPDATE_EVENT';
+export const DELETE_EVENT = 'DELETE_ENVET';
